2371cdd6866dc68f8e1d77021501736ed0617a04
d4140n-4h3-1 <55861113+d4140n-4h3-1@users.noreply.github.com>
2021-06-12 23:24:49 -0500

    Add files via upload

---
+++ generate_structure.sh
@@ -0,0 +1,30 @@
+mkdir Desktop Documents Downloads Music Pictures Templates Videos
+
+cd Documents
+	mkdir markdown office pdf programming scripting svg-roughdraft txt web
+	cd office
+		mkdir odf-math odg-draw odp-impress ods-calc odt-writer
+	cd ../programming
+		mkdir assembly c c++ crystal forth go haskell java lua ocaml parasail python ruby rust swift vala
+	cd ../web
+		mkdir css html javascript media php
+		cd media
+			mkdir audio photo video
+			cd audio
+				mkdir flac ogg
+			cd ../photo
+				mkdir gif png svg
+			cd ../video
+				mkdir webm
+
+cd ../../../../Music
+	mkdir flac mp3 ogg wav
+
+cd ../Pictures
+	mkdir jpeg png gif svg .icons # ICONS ARE MADE INVISIBLE SO THEY ARE NOT AN EYESORE FOR THE AVERAGE USER
+
+cd ../Templates
+	touch Untitled.odf Untitled.odg Untitled.odp Untitled.ods Untitled.odt Untitled.txt
+
+cd ../Videos
+	mkdir mov mp4 ogg webm
